6220ea9d3319a7a82f5bf878bd9f8b0e0a0e939e
Van-Excel <vanexcel.investments@gmail.com>
2023-04-15 11:03:16 +0000

    added login and logout

--- config/settings.py
+++ config/settings.py
@@ -124,3 +124,6 @@ STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]  # new
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'logout'
