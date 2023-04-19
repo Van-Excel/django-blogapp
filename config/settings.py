653239a11d9f48b724b98d4cb00555e872508f7c
Van-Excel <vanexcel.investments@gmail.com>
2023-04-19 11:31:12 +0000

    configured staticfiles

--- config/settings.py
+++ config/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]  # new
+STATIC_ROOT = STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))  # new
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'  # new
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
